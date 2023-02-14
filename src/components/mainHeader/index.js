5ee13b5ee545fc732b94a319ce965807cfe66841
Svetlana <swetaz@yandex.ru>
2023-02-14 21:18:28 +0300

    14.02.23

--- src/components/mainHeader/index.js
+++ src/components/mainHeader/index.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 export const MainMenu = () => {
     return (
+        <>
         <nav>
             <button>user</button>
             <button>contacts</button>
             <button>shop</button>
             <button>cart</button>
         </nav>
+        <Outlet/>
+        </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -10,28 +10,41 @@ import { Login } from './pages/login';
 import { Cart } from './pages/cart';
 import { NotFound } from './pages/notFound';
 import { MainMenu } from './components/mainHeader';
+import { Provider } from 'react-redux';
+import { store } from './redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // const [{products}] = data;
-root.render(
-    
-  <BrowserRouter>
-    <MainMenu/>
-    <Routes>      
-      <Route path='/' element={<Shop/>}/>
-      <Route path='/contacts' element={<Contacts/>}/>
-      <Route path='/cart' element={<Cart/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/shop' element={<Shop/>}/>
-      <Route path='/product/:productId' element={<Product/>}/>
-      <Route path='/product' element={<Navigate to='/'/>}/>
-      <Route path='*' element={<NotFound/>}/>
-      
-    </Routes>
-    {/* <Shop />
-    <Product/> */}
-  </BrowserRouter>
-  
+root.render(   
+  <Provider store={store}>
+    <BrowserRouter>
+      {/* <MainMenu/> */}
+      <Routes> 
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/' element={<MainMenu/>}>
+          {/* <Route path='/' element={<Shop/>}/>  можно двумя вариантами */}
+          <Route index element={<Shop/>}/>
+          <Route path='contacts' element={<Contacts/>}/>
+          <Route path='cart' element={<Cart/>}/>
+          <Route path='product/:productId' element={<Product/>}/>
+          <Route path='product' element={<Navigate to='/'/>}/>
+        </Route>     
+        <Route path='*' element={<NotFound/>}/>
+
+        {/* <Route path='/' element={<Shop/>}/>
+        <Route path='/contacts' element={<Contacts/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/shop' element={<Shop/>}/>
+        <Route path='/product/:productId' element={<Product/>}/>
+        <Route path='/product' element={<Navigate to='/'/>}/>
+        <Route path='*' element={<NotFound/>}/> */}
+        
+      </Routes>
+      {/* <Shop />
+      <Product/> */}
+    </BrowserRouter>
+  </Provider> 
 );
 
 
